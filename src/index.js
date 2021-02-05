4790be04e477eba48db1c56b40d985a95056bd5d
soniatheodoridou <soniatheodoridou@gmail.com>
2021-02-05 11:35:12 +0000

    Added icons

--- src/index.js
+++ src/index.js
@@ -71,7 +71,7 @@ function displayForecast(response) {
       />
       <div class="weather-forecast-temperature">
         <strong>
-          ${Math.round(forecast.main.temp_max)}°
+          ${Math.round(forecast.main.temp_max)}° / 
         </strong>
         ${Math.round(forecast.main.temp_min)}°
       </div>
